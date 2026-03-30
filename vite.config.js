1e22309feddae9debfa891346adb461e819f4c9f
Juste Raimbault <juste.raimbault@ign.fr>
2026-03-30 22:23:19 +0200

    fix base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
+import fs from 'fs';
+
+const config = JSON.parse(fs.readFileSync('./src/main/resources/config.json', 'utf-8'));
 
 export default defineConfig({
   plugins: [scalaJSPlugin()],
-  base: '/annotator/',
+  base: config.base,
 });
